32fb9105c89446414507e4c802d114b022c08ade
Michael Friendly <friendly@yorku.ca>
2022-06-16 14:35:19 -0400

    add rudimentary logo functions

---
+++ R/get_logo.R
@@ -0,0 +1,34 @@
+#' Get logo information for a teams
+#'
+#' @param teams a character vector of one or more `TeamID`s
+#' @return A data.frame with columns `teamID`, `name`, `png`
+#' @importFrom dplyr filter select
+#' @examples
+#' logoInfo(c("TOR", "TEX"))
+#'
+logoInfo <- function(teams) {
+  info <- Logos |>
+    dplyr::filter(teamID %in% teams) |>
+    dplyr::select(teamID, name, png)
+  info
+}
+
+#' Get logo images for teams
+#'
+#' @param teams
+#'
+#' @return the image files for the teams
+#' @importFrom magick image_read
+#' @export
+#'
+#' @examples
+#' toronto <- get_logo("TOR")
+#' plot(toronto)
+#'
+get_logo <- function(teams) {
+  images <- logoInfo(teams)
+  if (nrow(images) == 0) stop("No teamIDs found")
+  png <- images[,"png"]
+  dir <- system.file("png/", package = "MLBlogos")
+  magick::image_read(file.path(dir, png))
+}
